e0d7cd28062d904fb45c312ab78583399607cf3a
ralee25 <ralee@g.hmc.edu>
2025-02-26 23:13:54 -0800

    image check

--- src/visualize.py
+++ src/visualize.py
@@ -14,22 +14,31 @@ import json
 import matplotlib.pyplot as plt
 
 # open the input path
-with open(args.input_path, 'r', encoding='utf-8') as f:
+with open(args.input_path) as f:
     counts = json.load(f)
 
-top_items = sorted(counts[args.key].items(), key=lambda item: item[1])[:10]
-labels, values = zip(*top_items)
+if args.percent:
+    for k in counts[args.key]:
+        counts[args.key]/= counts['_all'][k]
+
+items = sorted(counts[args.key].items(), key=lambda item: (item[1], item[0]), reverse = True)
+top_items = items[:10]
+keys, values = zip(*top_items)
+
+input_filename = os.path.basename(args.input_path).replace(".json","")
+
 # create bar plot
-plt.figure(figsize=(10, 6))
-plt.barh(labels, values, color='skyblue')  # Horizontal bar chart
+plt.figure(figsize=(10, 5))
+plt.barh(keys, values, color='skyblue')  # Horizontal bar chart
 plt.xlabel('Count')
-plt.ylabel('Category')
-plt.title(f"Top 10 {args.key} Mentions in {os.path.basename(args.input_path)}")
-plt.grid(axis='x', linestyle='--', alpha=0.7)
+plt.ylabel('Language/Country')
+plt.title(f"Top 10 {args.key} Mentions in {input_filename}")
+plt.gca().invert_yaxis()
 
 # save the figure
-output_filename = f"{args.key}_{os.path.basename(args.input_path).replace('.json', '.png')}"
+output_filename = f"{input_filename}_{args.key.replace('#', '')}_top_10_bargraph.png"
 plt.savefig(output_filename, bbox_inches='tight')
+plt.close()
 print(f"Saved plot as {output_filename}")
 
 # normalize the counts by the total values
